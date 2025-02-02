292cef7cf561ed80983d840c510edf38c591327d
giangiffordd <gianalmario06@gmail.com>
2025-02-03 01:18:33 +0800

    Delete TS1.js

--- TS1.js
+++
@@ -1,8 +0,0 @@
-const colorButton = document.querySelector('.change-color');
-const colors = ['red', 'green', 'blue', 'white'];
-let currentIndex = 0;
-
-colorButton.addEventListener('click', () => {
-    document.body.style.backgroundColor = colors[currentIndex];
-    currentIndex = (currentIndex + 1) % colors.length;
-});
